57bbf6136833a705088437fb221ab68a2b75db2b
randallkk <randallkk.sw@gmail.com>
2023-05-03 03:57:37 +0900

    S08P31A606-210 feat: OAuth2 Client 방식 로그인 설정 중

---
+++ kkiri-be/src/main/java/com/lets/kkiri/config/auth/SecurityConfig.java
@@ -0,0 +1,90 @@
+package com.lets.kkiri.config.auth;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lets.kkiri.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+/**
+ * Spring Security 설정
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    // 인증이 필요없는 API 목록
+    private static final String[] OPEN_API_GET = new String[] {
+            "/api/auth/**",
+            "/api/contents/**",
+            "/api/search/**",
+            "/api/categories/**",
+            "/api/recommends/random/**",
+            "/api/quizzes/**",
+            "/login/**",
+            "/oauth2/**",
+            "/api/quizzes/**"
+    };
+
+    private static final String[] OPEN_API_POST = new String[] {
+            "/api/auth/signin/**",
+            "/api/contents/**/feedback",
+    };
+
+    private static final String[] ADMIN_API_POST = new String[] {
+            "/api/admin/**",
+    };
+
+    private final MemberAuthService memberAuthService;
+    private final MemberService memberService;
+    private final ObjectMapper objectMapper;
+    private final SavedRequestAwareAuthenticationSuccessHandler successHandler;
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 사용 하지않음
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, OPEN_API_GET).permitAll()
+                .antMatchers(HttpMethod.POST, OPEN_API_POST).permitAll()
+                .anyRequest().authenticated()
+                .and().cors()
+                .and()
+                // OAuth2 로그인 설정
+                .oauth2Login()          // OAuth2기반의 로그인인 경우
+                .userInfoEndpoint()     // OAuth2 로그인 성공 이후 사용자 정보를 가져올 때의 설정들을 담당
+                .userService(memberAuthService)  // 소셜 provider에서 인가 성공 시 후속 조치를 진행할 OAuth2UserService 인터페이스의 구현체를 등록
+                .and()
+                .successHandler(successHandler);    // 로그인 성공 시 후속 조치를 진행할 핸들러를 등록
+
+//        http
+//                .httpBasic().disable()
+//                .csrf().disable()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 사용 하지않음
+//                .and()
+////                .addFilter(new JwtAuthenticationFilter(authenticationManager(), memberService)) //HTTP 요청에 JWT 토큰 인증 필터를 거치도록 필터를 추가
+//                .authorizeRequests()
+//                .anyRequest().permitAll()
+//                .and().cors();
+        return http.build();
+    }
+
+}
---
+++ kkiri-be/src/main/java/com/lets/kkiri/service/AuthService.java
@@ -0,0 +1,92 @@
+package com.lets.kkiri.service;
+
+import com.lets.kkiri.dto.auth.OAuth2UserInfo;
+import com.lets.kkiri.dto.auth.OAuthAttributes;
+import com.lets.kkiri.entity.Member;
+import com.lets.kkiri.entity.Role;
+import com.lets.kkiri.repository.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class AuthService implements OAuth2UserService {
+
+    private final OAuth2UserInfo oAuth2UserInfo;
+    private final MemberRepository memberRepository;
+
+    @Override
+    @Transactional
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        // OAuth2 서비스 제공자 구분
+        String provider = userRequest.getClientRegistration().getRegistrationId();
+
+        // OAuth2 계정 정보 가져오기
+        DefaultOAuth2UserService oAuth2UserService = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = oAuth2UserService.loadUser(userRequest);
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+        OAuthAttributes oAuthAttributes = OAuthAttributes.of(provider, userNameAttributeName, attributes);
+        log.info("oAuthAttributes: {}", oAuthAttributes);
+
+        Member member = saveOrUpdate(provider, oAuthAttributes.getOAuth2UserInfo());
+        log.debug("member: {}", member.toString());
+
+        // TODO CustonOAuth2User으로 바꿔서 쿼리 한번 덜 날리게 리팩토링
+        return new DefaultOAuth2User(
+                Collections.singleton(new SimpleGrantedAuthority(member.getRole().toString())),
+                oAuthAttributes.getOAuth2UserInfo().getAttributes(),
+                oAuthAttributes.getUserNameAttributeName()
+        );
+    }
+
+    @Transactional
+    public Member saveOrUpdate(String provider, OAuth2UserInfo oAuth2UserInfo) {
+
+        Member oAuthMember = Member.builder()
+                .email(oAuth2UserInfo.getEmail())
+                .role(Role.ROLE_MEMBER)
+                .build();
+
+        Optional<Member> findMember =  memberRepository.findByEmail(oAuthMember.getEmail());
+        log.debug("oAuthMember.getIdentifier: {}", oAuthMember.getEmail());
+        Member member;
+        if (findMember.isPresent()) {
+            log.info("findMember.isPresent: 이미 회원가입한 적 있는 회원입니다.");
+            // TODO: 회원가입한 적 있는 회원이면 소셜 로그인 방식 확인 후 소셜 로그인 방식이 다르면 예외처리 (이미 다른 소셜 로그인 방식으로 가입한 회원입니다.)
+            throw new RuntimeException("이미 다른 소셜 로그인 방식으로 가입한 회원입니다.");
+        } else {
+            log.info("findMember.isNOTPresent: 회원가입한 적 없는 회원입니다.");
+            member = memberRepository.save(oAuthMember);
+        }
+        log.debug("member: {}", member.toString());
+        return member;
+    }
+
+    @Override
+    public Member getMember(String token) {
+        String identifier = JwtTokenUtil.getIdentifier(token);
+        Optional<Member> findMember = memberRepository.findByEmail(identifier);
+        if (findMember.isPresent()) {
+            return findMember.get();
+        } else{
+            throw new MemberNotFoundException();
+        }
+    }
+
+
+}
