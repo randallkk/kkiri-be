e2ba0ce302c8d56d2b3429afbb1e18a720eaf775
randallkk <randallkk.sw@gmail.com>
2023-05-03 03:54:26 +0900

    S08P31A606-210 feat: OAuth2UserInfo, 구현체 DTO 생성

---
+++ kkiri-be/src/main/java/com/lets/kkiri/dto/auth/GoogleOAuth2UserInfo.java
@@ -0,0 +1,29 @@
+package com.lets.kkiri.dto.auth;
+
+import java.util.Map;
+
+public class GoogleOAuth2UserInfo extends OAuth2UserInfo{
+    public GoogleOAuth2UserInfo(Map<String, Object> attributes) {
+        super(attributes);
+    }
+
+    @Override
+    public String getId() {
+        return attributes.get("sub").toString();
+    }
+
+    @Override
+    public String getName() {
+        return attributes.get("name").toString();
+    }
+
+    @Override
+    public String getEmail() {
+        return attributes.get("email").toString();
+    }
+
+    @Override
+    public String getProvider() {
+        return "google";
+    }
+}
