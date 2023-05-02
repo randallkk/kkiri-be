24fc21497c20d0cf3229ee68912e4054ca1d617b
randallkk <randallkk.sw@gmail.com>
2023-05-03 03:57:59 +0900

    S08P31A606-210 feat: MemberNotFoundException 생성

---
+++ kkiri-be/src/main/java/com/lets/kkiri/common/exception/member/MemberNotFoundException.java
@@ -0,0 +1,17 @@
+package com.lets.kkiri.common.exception.member;
+
+
+public class MemberNotFoundException extends RuntimeException{
+    public MemberNotFoundException() {
+        super("존재하지 않는 회원입니다.");
+    }
+
+    public MemberNotFoundException(String message) {
+        super(message);
+    }
+
+    public MemberNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
