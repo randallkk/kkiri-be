c746f2399245e22db8f33ae8aeafc5c7658ff020
이지영 <jylisa0612@naver.com>
2023-05-02 17:00:14 +0900

    S08P31A606-122-be-채팅-api-작성 feat: chatDto 작성

---
+++ kkiri-be/src/main/java/com/lets/kkiri/dto/chatting/ChatDto.java
@@ -0,0 +1,25 @@
+package com.lets.kkiri.dto.chatting;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ChatDto {
+	private Long id;
+	private Long chatRoomId;
+	private Long memberId;
+	private String message;
+	private String region;
+	@JsonDeserialize(using = LocalDateDeserializer.class)
+	private LocalDateTime regDate;
+}
