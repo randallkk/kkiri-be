f3774f5197fb44b85480db723941a9ddf3c33586
7unho <rlawnsgh8395@naver.com>
2023-05-03 15:51:28 +0900

    feat: MEMBER_DEVIC 엔티티 작성

---
+++ kkiri-be/src/main/java/com/lets/kkiri/entity/MemberDevice.java
@@ -0,0 +1,25 @@
+package com.lets.kkiri.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class MemberDevice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+    private String token;
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
