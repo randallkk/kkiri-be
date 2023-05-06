a2fd9b9f150c4c4509f3d66a0abb1236b8e4100f
7unho <rlawnsgh8395@naver.com>
2023-05-06 16:04:29 +0900

    feat: 도움 요청 api 작성

--- kkiri-be/src/main/java/com/lets/kkiri/repository/member/MemberDeviceRepositorySupport.java
+++ kkiri-be/src/main/java/com/lets/kkiri/repository/member/MemberDeviceRepositorySupport.java
@@ -3,6 +3,7 @@ package com.lets.kkiri.repository.member;
 //import com.lets.kkiri.entity.QMemberDevice;
 //import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.lets.kkiri.entity.QMemberDevice;
+import com.lets.kkiri.entity.QMemberTopic;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.query.JpaQueryMethodFactory;
@@ -18,11 +19,23 @@ public class MemberDeviceRepositorySupport {
     private final JPAQueryFactory jpaQueryFactory;
 
     QMemberDevice qMemberDevice = QMemberDevice.memberDevice;
+    QMemberTopic qMemberTopic = QMemberTopic.memberTopic;
 
     public List<String> findTokenListByMemberId(Long memberId) {
         return jpaQueryFactory.select(qMemberDevice.token)
                 .from(qMemberDevice)
                 .where(qMemberDevice.member.id.eq(memberId))
+                .fetch();
+    }
+
+    public List<String> findTokenListByMoimId(Long senderId, String moimId) {
+        return jpaQueryFactory.select(qMemberDevice.token)
+                .from(qMemberDevice, qMemberTopic)
+                .where(
+                        qMemberDevice.member.id.ne(senderId),
+                        qMemberTopic.name.eq(moimId),
+                        qMemberDevice.id.eq(qMemberTopic.memberDevice.id)
+                )
                 .fetch();
     }
 }
