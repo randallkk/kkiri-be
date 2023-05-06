81c39bf72cd94e257e591e71c5e0ffc5e4e1b81a
7unho <rlawnsgh8395@naver.com>
2023-05-06 16:36:02 +0900

    feat: 경로 전송 API 작성, request dto 작성 senderEmail, recieverEmail, path[]

---
+++ kkiri-be/src/main/java/com/lets/kkiri/dto/noti/RouteGuideNotiReq.java
@@ -0,0 +1,20 @@
+package com.lets.kkiri.dto.noti;
+
+import com.lets.kkiri.dto.gps.PointDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Builder
+@AllArgsConstructor
+@Data
+public class RouteGuideNotiReq {
+    // 발신 유저 이메일
+    private String senderEmail;
+
+    // 수신 유저 이메일
+    private String recieverEmail;
+    private List<PointDto> path;
+}
