854a28b1b5012e749a18dcc00f0be94f17f3d3e1
EveKritinLee <jylisa0612@naver.com>
2023-05-08 04:19:39 +0900

    S08P31A606-122-be-채팅-api-작성 feat : 채팅 연결

--- kkiri-be/src/main/java/com/lets/kkiri/controller/ChatController.java
+++ kkiri-be/src/main/java/com/lets/kkiri/controller/ChatController.java
@@ -2,6 +2,8 @@ package com.lets.kkiri.controller;
 
 import java.time.LocalDateTime;
 
+import com.lets.kkiri.entity.MongoMessage;
+import com.lets.kkiri.service.ChatService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -13,6 +15,9 @@ import com.lets.kkiri.dto.chatting.ChatDto;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -22,20 +27,52 @@ public class ChatController {
 	@Qualifier("chatRabbitTemplate")
 	private final RabbitTemplate template;
 
+	private final ChatService chatService;
+
 	private final static String CHAT_EXCHANGE_NAME = "chat.exchange";
 	private final static String CHAT_QUEUE_NAME = "chat.queue";
 
-	@MessageMapping("chat.enter.{chatRoomId}")
-	public void enter(ChatDto chatDto, @DestinationVariable String chatRoomId) {
+	@MessageMapping("chat.enter.{roomId}")
+	public void enter(ChatDto chatDto, @DestinationVariable String roomId) {
 		chatDto.setMessage("입장하셨습니다.");
 		chatDto.setRegDate(LocalDateTime.now());
 
 		//exchange
-		template.convertAndSend(CHAT_EXCHANGE_NAME, "room."+chatRoomId, chatDto);
+		template.convertAndSend(CHAT_EXCHANGE_NAME, "room."+roomId, chatDto); //exchange
+
+		//DB에 넣기
+		chatService.chatInsert(MongoMessage.builder()
+						.message(chatDto.getMessage())
+						.time(chatDto.getRegDate())
+//						.memberId(chatDto.getMemberId())
+//						.roomId(chatDto.getRoomId())
+						.build());
 	}
 
+	@MessageMapping("chat.message.{roomId}")
+	public void send(ChatDto chatDto, @DestinationVariable String roomId) {
+		chatDto.setRegDate(LocalDateTime.now());
+
+		template.convertAndSend(CHAT_EXCHANGE_NAME, "room."+roomId, chatDto); //exchange
+	}
+
+	//receive()는 단순히 큐에 들어온 메시지를 소비만 한다(디버그용)
 	@RabbitListener(queues = CHAT_QUEUE_NAME)
 	public void receive(ChatDto chatDto){
 		log.info("chatDto.getMessage() = {}", chatDto.getMessage());
 	}
+
+//	@PostMapping("/chat")
+//	public void chat(@RequestBody ChatDto chatDto) {
+//		chatDto.setMessage("입장하셨습니다.");
+//		chatDto.setRegDate(LocalDateTime.now());
+//
+//		chatService.mongoInsert(MongoMessage.builder()
+//				.message(chatDto.getMessage())
+//				.time(chatDto.getRegDate())
+////						.memberId(chatDto.getMemberId())
+////						.roomId(chatDto.getRoomId())
+//				.build());
+//	}
+
 }
