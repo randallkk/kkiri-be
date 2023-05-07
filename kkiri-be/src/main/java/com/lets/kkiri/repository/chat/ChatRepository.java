7571bf84f65da3b23af47a8afcc222eecd1fdbe6
EveKritinLee <jylisa0612@naver.com>
2023-05-08 04:18:50 +0900

    S08P31A606-122-be-채팅-api-작성 feat : mongoDB 연결

--- kkiri-be/src/main/java/com/lets/kkiri/repository/chat/ChatRepository.java
+++ kkiri-be/src/main/java/com/lets/kkiri/repository/chat/ChatRepository.java
@@ -1,4 +1,10 @@
 package com.lets.kkiri.repository.chat;
 
-public interface ChatRepository {
+import com.lets.kkiri.entity.Message;
+import com.lets.kkiri.entity.MongoMessage;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChatRepository extends MongoRepository<MongoMessage, String> {
 }
