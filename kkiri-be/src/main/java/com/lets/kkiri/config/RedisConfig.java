d1b3b72e00af0d434e2021d5f1fca64008c357d3
7unho <rlawnsgh8395@naver.com>
2023-05-09 16:45:23 +0900

    feat: Spring Security, JWT Config, Redis config 작성

---
+++ kkiri-be/src/main/java/com/lets/kkiri/config/RedisConfig.java
@@ -0,0 +1,46 @@
+package com.lets.kkiri.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/*
+        redis를 사용하기 위한 설정들을 입력해준다.
+ */
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+    @Value("${spring.redis.host}")
+    private String REDIS_HOST;
+
+    @Value("${spring.redis.port}")
+    private int REDIS_PORT;
+
+    @Value("${spring.redis.password}")
+    private String REDIS_PASSWORD;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisConfig =  new RedisStandaloneConfiguration();
+        redisConfig.setHostName(REDIS_HOST);
+        redisConfig.setPort(REDIS_PORT);
+        redisConfig.setPassword(REDIS_PASSWORD);
+        return new LettuceConnectionFactory(redisConfig);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+        // 아래 두 라인을 작성하지 않으면, key값이 \xac\xed\x00\x05t\x00\x03sol 이렇게 조회된다.
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+}
