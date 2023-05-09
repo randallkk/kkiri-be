b88f63792664832b5a306f54394ea8a9d7a7db21
이지영 <jylisa0612@naver.com>
2023-05-09 17:57:09 +0900

    S08P31A606-278-be-web-socket-only-설정-확인 feat: websocket 환경설정 구현중

--- kkiri-be/src/main/java/com/lets/kkiri/controller/ChatController.java
+++ kkiri-be/src/main/java/com/lets/kkiri/controller/ChatController.java
@@ -11,6 +11,7 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.lets.kkiri.dto.chatting.ChatDto;
 
@@ -22,65 +23,9 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class ChatController {
 
-	@Qualifier("chatRabbitTemplate")
-	private final RabbitTemplate template;
-
-	private final ChatService chatService;
-
-	private final static String CHAT_EXCHANGE_NAME = "chat.exchange";
-
-
-	@MessageMapping("/chat/enter/{roomId}")
-	@SendTo("/topic/room.{roomId}")
-	public void enter(
-		ChatDto chatDto,
-		@DestinationVariable String roomId) {
-		chatDto.setMessage("입장하셨습니다!!");
-		chatDto.setRegDate(LocalDateTime.now());
-		System.out.println("==========1===============");
-		receive(chatDto);
-
-		//exchange
-		template.convertAndSend(CHAT_EXCHANGE_NAME, "room."+roomId, chatDto); //exchange
-		System.out.println("==========2===============");
-		receive(chatDto);
-
-		//DB에 넣기
-		chatService.chatInsert(Message.builder()
-			.message(chatDto.getMessage())
-			.time(chatDto.getRegDate())
-			.memberId(chatDto.getMemberId())
-			//						.roomId(chatDto.getRoomId())
-			.build());
+	@GetMapping("/chat")
+	public String chatGet() {
+		log.info("@ChatController, chat GET()");
+		return
 	}
-
-	private final static String CHAT_QUEUE_NAME = "chat.queue";
-
-	@MessageMapping("/chat/message/{roomId}")
-	public void send(ChatDto chatDto, @DestinationVariable String roomId) {
-		chatDto.setRegDate(LocalDateTime.now());
-
-		receive(chatDto);
-		template.convertAndSend(CHAT_EXCHANGE_NAME, "room."+roomId, chatDto); //exchange
-	}
-
-	//receive()는 단순히 큐에 들어온 메시지를 소비만 한다(디버그용)
-	@RabbitListener(queues = CHAT_QUEUE_NAME)
-	public void receive(ChatDto chatDto){
-		log.info("chatDto.getMessage() = {}", chatDto.getMessage());
-	}
-
-//	@PostMapping("/chat")
-//	public void chat(@RequestBody ChatDto chatDto) {
-//		chatDto.setMessage("입장하셨습니다.");
-//		chatDto.setRegDate(LocalDateTime.now());
-//
-//		chatService.mongoInsert(MongoMessage.builder()
-//				.message(chatDto.getMessage())
-//				.time(chatDto.getRegDate())
-////						.memberId(chatDto.getMemberId())
-////						.roomId(chatDto.getRoomId())
-//				.build());
-//	}
-
 }
--- kkiri-be/src/main/java/com/lets/kkiri/service/ChatService.java
+++ kkiri-be/src/main/java/com/lets/kkiri/service/ChatService.java
@@ -1,5 +1,7 @@
 package com.lets.kkiri.service;
 
+import javax.websocket.server.ServerEndpoint;
+
 import com.lets.kkiri.entity.Message;
 import com.lets.kkiri.repository.chat.ChatRepository;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
-@RequiredArgsConstructor
 @Service
 public class ChatService {
 
-//    private final MongoTemplate mongoTemplate;
-    private final ChatRepository chatRepository;
-
-    public void chatInsert(Message mongoMessage) {
-//        mongoTemplate.insert(mongoMessage);
-        chatRepository.insert(mongoMessage);
-    }
 }
