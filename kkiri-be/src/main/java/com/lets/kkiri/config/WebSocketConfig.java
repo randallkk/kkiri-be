29257e60019f8a0cfd18790a8d85b55bcee071a4
EveKritinLee <jylisa0612@naver.com>
2023-05-10 01:17:21 +0900

    S08P31A606-278-be-web-socket-only-설정-확인 feat : WebSocketConfig 설정

--- kkiri-be/src/main/java/com/lets/kkiri/config/WebSocketConfig.java
+++ kkiri-be/src/main/java/com/lets/kkiri/config/WebSocketConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,11 +12,11 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
-	private final ChatHandler chatHandler;
+	private final MessageHandler messageHandler;
 
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-		registry.addHandler(chatHandler, "/ws")
+		registry.addHandler(messageHandler, "/ws/api")
 			.setAllowedOrigins("*")
 		;
 	}
