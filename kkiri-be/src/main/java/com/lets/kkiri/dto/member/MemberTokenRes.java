f4bd356d6701857f2961fd3a9e3a260c38f65d8b
7unho <rlawnsgh8395@naver.com>
2023-05-10 21:50:44 +0900

    feat: MemberTokenRes 디바이스 토큰 리스트 필드 추가

--- kkiri-be/src/main/java/com/lets/kkiri/dto/member/MemberTokenRes.java
+++ kkiri-be/src/main/java/com/lets/kkiri/dto/member/MemberTokenRes.java
@@ -3,9 +3,12 @@ package com.lets.kkiri.dto.member;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.List;
+
 @Builder
 @Data
 public class MemberTokenRes {
     String accessToken;
     String refreshToken;
+    List<String> deviceTokens;
 }
