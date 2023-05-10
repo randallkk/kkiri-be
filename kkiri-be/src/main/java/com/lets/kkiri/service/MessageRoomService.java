4808a4d64a53284d0e73354e16a7cae300df85be
EveKritinLee <jylisa0612@naver.com>
2023-05-11 05:40:15 +0900

    S08P31A606-278-be-web-socket-only-설정-확인 feat : 채팅방의 session에 메시지 발송 로직 구현중

---
+++ kkiri-be/src/main/java/com/lets/kkiri/service/MessageRoomService.java
@@ -0,0 +1,28 @@
+package com.lets.kkiri.service;
+
+import com.lets.kkiri.dto.chatting.MessageDto;
+import com.lets.kkiri.dto.moim.MoimSessionListDto;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.util.Set;
+
+@Getter
+@Setter
+public class MessageRoomService {
+    MoimSessionListDto moimSessionListDto;
+    
+    public void handleActions(WebSocketSession session, Object content, MessageService messageService) {    
+
+        content = (MessageDto) content;
+
+        sendMessage(content, messageService);
+    }
+
+    public <T> void sendMessage(T content, MessageService messageService) {
+        Set<WebSocketSession> sessions = moimSessionListDto.getSessions();
+        sessions.parallelStream().forEach(session -> messageService.sendMessage(session, content));
+    }
+
+}
