aa134d59f18997f3163cfade612f7e2bd41ca0dc
이지영 <jylisa0612@naver.com>
2023-05-11 15:50:31 +0900

    S08P31A606-278-be-web-socket-only-설정-확인 feat: 변수명 수정

--- kkiri-be/src/main/java/com/lets/kkiri/dto/chatting/MessageRes.java
+++ kkiri-be/src/main/java/com/lets/kkiri/dto/chatting/MessageRes.java
@@ -2,7 +2,16 @@ package com.lets.kkiri.dto.chatting;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MessageRes {
-	private MessageMetaData meata;
+	private MessageMetaData meta;
 	private List<MessageSub> chat;
 }
