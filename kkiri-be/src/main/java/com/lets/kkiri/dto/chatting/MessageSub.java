b52687fe4f8d98784b5259f500a83c7ca412f1e0
이지영 <jylisa0612@naver.com>
2023-05-11 15:52:54 +0900

    S08P31A606-278-be-web-socket-only-설정-확인 feat: Message to Dto 메소드 구현

--- kkiri-be/src/main/java/com/lets/kkiri/dto/chatting/MessageSub.java
+++ kkiri-be/src/main/java/com/lets/kkiri/dto/chatting/MessageSub.java
@@ -4,12 +4,12 @@ import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.lets.kkiri.entity.Message;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 @Builder
@@ -23,5 +23,14 @@ public class MessageSub {
 	private String nickname;
 	private String message;
 	@JsonDeserialize(using = LocalDateDeserializer.class)
-	private LocalDateTime regDate;
+	private LocalDateTime time;
+
+	public static MessageSub toDto(Message msg) {
+		return new MessageSub(
+			msg.getMoimId(),
+			msg.getMemberKakaoId(),
+			msg.getNickname(),
+			msg.getMessage(),
+			msg.getTime());
+	}
 }
