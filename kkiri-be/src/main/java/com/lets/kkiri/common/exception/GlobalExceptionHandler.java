d72f9475ca7526b57339e3956077b81a32a94d1b
7unho <rlawnsgh8395@naver.com>
2023-05-11 14:54:42 +0900

    feat: GlobalExceptionHandler 작성

--- kkiri-be/src/main/java/com/lets/kkiri/common/exception/GlobalExceptionHandler.java
+++ kkiri-be/src/main/java/com/lets/kkiri/common/exception/GlobalExceptionHandler.java
@@ -1,4 +1,28 @@
 package com.lets.kkiri.common.exception;
 
-public class ExceptionAdvice {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.io.FileNotFoundException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({KkiriException.class})
+    protected ResponseEntity handleKkiriException(KkiriException e) {
+        return new ResponseEntity(e.getErrorCode(), e.getErrorCode().getStatus());
+    }
+    @ExceptionHandler({UnsupportedOperationException.class})
+    protected ResponseEntity handleUnsupportedOperationException(UnsupportedOperationException e) {
+        e.printStackTrace();
+        return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler({FileNotFoundException.class})
+    protected ResponseEntity handleFileNotFoundException(FileNotFoundException e) {
+        e.printStackTrace();
+        return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
