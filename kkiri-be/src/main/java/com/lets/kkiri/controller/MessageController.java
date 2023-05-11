6b99cf5867acead9bdcd642eae46b608553331c6
이지영 <jylisa0612@naver.com>
2023-05-11 15:45:24 +0900

    S08P31A606-278-be-web-socket-only-설정-확인 feat: 채팅 이력 조회하기 구현

--- kkiri-be/src/main/java/com/lets/kkiri/controller/MessageController.java
+++ kkiri-be/src/main/java/com/lets/kkiri/controller/MessageController.java
@@ -6,13 +6,11 @@ import com.lets.kkiri.service.MessageService;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,12 +24,14 @@ public class MessageController {
 	private final MessageService messageService;
 	private final MessageRoomService messageRoomService;
 
-	@GetMapping
+	@GetMapping()
 	public MessageRes getChat(@RequestParam Long moimId,
-		@RequestParam(defaultValue = "999999") String lastMessageId,
+		@RequestParam(required = false) String lastMessageId,
 		@PageableDefault(size=30, page = 0) Pageable pageable) {
-		// return MessageRoomService.getChat(moimId, );
-		return null;
+		if(lastMessageId == null)
+			return messageRoomService.getFirstChat(moimId, pageable);
+		else
+			return messageRoomService.getChat(moimId, lastMessageId, pageable);
 	}
 
 
