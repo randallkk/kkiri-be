5ca95bc5c4c79b07fe63594d3e9d2a59066ce321
randallkk <randallkk.sw@gmail.com>
2023-05-11 14:50:02 +0900

    S08P31A606-215 fix: websocket 연결 paths 변경

--- kkiri-be/src/main/java/com/lets/kkiri/config/websocket/WebSocketConfig.java
+++ kkiri-be/src/main/java/com/lets/kkiri/config/websocket/WebSocketConfig.java
@@ -15,7 +15,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-		registry.addHandler(moimSessionHandler, "/ws/api")
+		registry.addHandler(moimSessionHandler, "/ws/api/{moimId}")
 				.addInterceptors(new HandshakeInterceptor())
 			.setAllowedOrigins("*");
 	}
