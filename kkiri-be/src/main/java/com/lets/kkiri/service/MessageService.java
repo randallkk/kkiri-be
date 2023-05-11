eeaa09058237a4f8dfc12889d95a7ab4ed676cf7
이지영 <jylisa0612@naver.com>
2023-05-11 15:52:16 +0900

    S08P31A606-278-be-web-socket-only-설정-확인 feat: 채팅 전송 구현

--- kkiri-be/src/main/java/com/lets/kkiri/service/MessageService.java
+++ kkiri-be/src/main/java/com/lets/kkiri/service/MessageService.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -20,10 +21,13 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class MessageService {
     private final ObjectMapper objectMapper;
+    private final MongoTemplate mongoTemplate;
 
     public void sendMessage(WebSocketSession session, Object content)  {
         try{
             session.sendMessage(new TextMessage(objectMapper.writeValueAsString(content)));
+            log.debug("sendMessage : {}", content);
+            // mongoTemplate.save(content);
         }catch (Exception e){
             log.debug("sendMessage error : {}", e.getMessage());
         }
