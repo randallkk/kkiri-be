926760d44af6951e34052a8028e3041f2a7a8d89
이지영 <jylisa0612@naver.com>
2023-05-11 10:24:22 +0900

    S08P31A606-278-be-web-socket-only-설정-확인 feat: 채팅 조회 관련 DTO 정의

---
+++ kkiri-be/src/main/java/com/lets/kkiri/dto/chatting/MessageMetaData.java
@@ -0,0 +1,13 @@
+package com.lets.kkiri.dto.chatting;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class MessageMetaData {
+	Long lastMessageId;
+	boolean last;
+}
