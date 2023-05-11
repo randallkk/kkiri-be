ceb413940d0bc9f55cd713a035e0b6f007189118
7unho <rlawnsgh8395@naver.com>
2023-05-11 14:17:23 +0900

    fix: NOTI API API 요청 로직에 JWT 헤더 추가

--- kkiri-be/src/main/java/com/lets/kkiri/controller/NotiController.java
+++ kkiri-be/src/main/java/com/lets/kkiri/controller/NotiController.java
@@ -1,5 +1,6 @@
 package com.lets.kkiri.controller;
 
+import com.lets.kkiri.config.jwt.JwtTokenUtil;
 import com.lets.kkiri.dto.noti.HelpNotiReq;
 import com.lets.kkiri.dto.noti.PressNotiReq;
 import com.lets.kkiri.dto.noti.RouteGuideNotiReq;
@@ -9,10 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.Response;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -21,20 +19,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class NotiController {
     private final NotiService notiService;
     @PostMapping("/presses")
-    public ResponseEntity<?> sendPressNoti(@RequestBody PressNotiReq pressNotiReq) {
-        notiService.sendPressNoti(pressNotiReq);
+    public ResponseEntity<?> sendPressNoti(
+            @RequestHeader(JwtTokenUtil.HEADER_STRING) String token,
+            @RequestBody PressNotiReq pressNotiReq
+    ) {
+        String senderKakaoId = JwtTokenUtil.getIdentifier(token);
+        notiService.sendPressNoti(senderKakaoId, pressNotiReq.getReceiverKakaoId());
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/helps")
-    public ResponseEntity<?> sendHelpNoti(@RequestBody HelpNotiReq helpNotiReq) {
-        notiService.sendHelpNoti(helpNotiReq);
+    public ResponseEntity<?> sendHelpNoti(
+            @RequestHeader(JwtTokenUtil.HEADER_STRING) String token,
+            @RequestBody HelpNotiReq helpNotiReq
+    ) {
+        String senderKakaoId = JwtTokenUtil.getIdentifier(token);
+        notiService.sendHelpNoti(senderKakaoId, helpNotiReq.getChatRoomId());
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/helps/guides")
-    public ResponseEntity<?> sendRouteGuide(@RequestBody RouteGuideNotiReq routeGuideReq) {
-        notiService.sendRoute(routeGuideReq);
+    public ResponseEntity<?> sendRouteGuide(
+            @RequestHeader(JwtTokenUtil.HEADER_STRING) String token,
+            @RequestBody RouteGuideNotiReq routeGuideReq)
+    {
+        String senderKakaoId = JwtTokenUtil.getIdentifier(token);
+        notiService.sendRoute(senderKakaoId, routeGuideReq);
         return ResponseEntity.ok().build();
     }
 }
