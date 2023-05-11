c36e6ec70282169ec46487fdbd5229c839a5812f
이지영 <jylisa0612@naver.com>
2023-05-11 17:48:58 +0900

    S08P31A606-278-be-web-socket-only-설정-확인 feat: MessageType마다 분기처리 구현중

--- kkiri-be/src/main/java/com/lets/kkiri/service/MessageRoomService.java
+++ kkiri-be/src/main/java/com/lets/kkiri/service/MessageRoomService.java
@@ -1,18 +1,23 @@
 package com.lets.kkiri.service;
 
+import com.lets.kkiri.common.util.RedisStoreUtil;
+import com.lets.kkiri.dto.chatting.MessageDto;
 import com.lets.kkiri.dto.chatting.MessageMetaData;
 import com.lets.kkiri.dto.chatting.MessagePub;
 import com.lets.kkiri.dto.chatting.MessageRes;
 import com.lets.kkiri.dto.chatting.MessageSub;
 import com.lets.kkiri.dto.moim.MoimSessionListDto;
+import com.lets.kkiri.dto.moim.MoimSessionReq;
 import com.lets.kkiri.entity.Message;
 import com.lets.kkiri.repository.chatting.MessageRepositorySupport;
 
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.WebSocketSession;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -25,18 +30,39 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class MessageRoomService {
     MoimSessionListDto moimSessionListDto;
+    private final RedisStoreUtil redisStoreUtil;
+
     private final MessageRepositorySupport messageRepositorySupport;
-    
-    public void handleActions(WebSocketSession session, Object content, MessageService messageService) {    
 
-        content = (MessagePub) content;
+    public void handleActions(WebSocketSession session, MoimSessionReq.MoimSessionType type, Object content, MessageService messageService) {
+        MessageDto msg = null;
+        switch (type) {
+            case MESSAGE:
+                msg = (MessageDto) content;
+                break;
+            case EMOJI:
+                msg = (MessageDto) content;
+                break;
+            case URGENT:
+                msg = (MessageDto) content;
+                msg.setMessage(msg.getNickname() + "님이 재촉하셨습니다.");
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + type);
+        }
 
-        sendMessage(content, messageService);
+        if(msg != null) {
+            sendMessage(session, type, msg, messageService);
+        }
+        else {
+            throw new IllegalStateException("message null");
+        }
     }
 
-    public <T> void sendMessage(T content, MessageService messageService) {
-        Set<WebSocketSession> sessions = moimSessionListDto.getSessions();
-        sessions.parallelStream().forEach(session -> messageService.sendMessage(session, content));
+    public <T> void sendMessage(WebSocketSession session, MoimSessionReq.MoimSessionType type, MessageDto msg, MessageService messageService) {
+        // Set<WebSocketSession> sessions = moimSessionListDto.getSessions();
+        ArrayList<WebSocketSession> sessions = redisStoreUtil.getAllSessionsByMoimId(Long.parseLong(session.getAttributes().get("moimId").toString()), WebSocketSession.class);
+        sessions.parallelStream().forEach(s -> messageService.sendMessage(s, msg));
     }
 
     public MessageRes getFirstChat(Long moimId, Pageable pageable){
