2471be3097994665809fc4dbe2e92db5db7b126f
7unho <rlawnsgh8395@naver.com>
2023-05-12 10:49:41 +0900

    feat:/api/auth 관련 에러 핸들링 적용

--- kkiri-be/src/main/java/com/lets/kkiri/config/auth/SecurityConfig.java
+++ kkiri-be/src/main/java/com/lets/kkiri/config/auth/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     };
 
     private static final String[] OPEN_API_POST = new String[]{
-            "/api/auth/signin/**",
+            "/api/auth/login",
             "/api/contents/**/feedback",
     };
 
@@ -85,7 +85,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .addFilter(new JwtAuthenticationFilter(authenticationManager(), memberService, redisTemplate)) //HTTP 요청에 JWT 토큰 인증 필터를 거치도록 필터를 추가
                 .authorizeRequests()
-                .anyRequest().permitAll()
+                .antMatchers(HttpMethod.GET, OPEN_API_GET).permitAll()
+                .antMatchers(HttpMethod.POST, OPEN_API_POST).permitAll()
+                .anyRequest().authenticated()
                 .and().cors();
     }
 
