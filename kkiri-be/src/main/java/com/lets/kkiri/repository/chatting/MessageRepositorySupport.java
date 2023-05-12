6d75d2d61b079316d3dac68ae0720bae149abf96
이지영 <jylisa0612@naver.com>
2023-05-12 14:18:51 +0900

    S08P31A606-278-be-web-socket-only-설정-확인 feat: 채팅 저장 및 조회

--- kkiri-be/src/main/java/com/lets/kkiri/repository/chatting/MessageRepositorySupport.java
+++ kkiri-be/src/main/java/com/lets/kkiri/repository/chatting/MessageRepositorySupport.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import com.lets.kkiri.entity.Message;
 
+import org.bson.types.ObjectId;
+import org.hibernate.annotations.Where;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -16,9 +18,11 @@ import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Repository
 @RequiredArgsConstructor
+@Slf4j
 public class MessageRepositorySupport {
 
 	private final MongoTemplate mongoTemplate;
@@ -26,18 +30,26 @@ public class MessageRepositorySupport {
 	public Page<Message> findRecent(Long moimId, Pageable pageable) {
 		Query query = new Query();
 		query.addCriteria(Criteria.where("moimId").is(moimId));
-		query.with(Sort.by(Sort.Direction.DESC, "time"));
+		query.with(Sort.by(Sort.Direction.DESC, "time").and(Sort.by(Sort.Direction.DESC, "_id")));
 		query.with(pageable);
-		List<Message> messages = null;
-		try {
-			messages = mongoTemplate.find(query, Message.class);
-		} catch (Exception ex) {
-		}
-			return PageableExecutionUtils.getPage(
+		List<Message> messages = mongoTemplate.find(query, Message.class);
+		return PageableExecutionUtils.getPage(
+		messages,
+		pageable,
+		() -> mongoTemplate.count(Query.of(query).limit(-1).skip(-1), Message.class)
+		);
+	}
+
+	public Page<Message> findMessage(Long moimId, Pageable pageable, String lastMessageId) {
+		Query query = new Query();
+		query.addCriteria(Criteria.where("_id").lt(new ObjectId(lastMessageId)).and("moimId").is(moimId));
+		query.with(Sort.by(Sort.Direction.DESC, "time").and(Sort.by(Sort.Direction.DESC, "_id")));
+		query.with(pageable);
+		List<Message> messages = mongoTemplate.find(query, Message.class);
+		return PageableExecutionUtils.getPage(
 			messages,
 			pageable,
 			() -> mongoTemplate.count(Query.of(query).limit(-1).skip(-1), Message.class)
 		);
 	}
-
 }
--- kkiri-be/src/main/java/com/lets/kkiri/service/MessageRoomService.java
+++ kkiri-be/src/main/java/com/lets/kkiri/service/MessageRoomService.java
@@ -1,5 +1,7 @@
 package com.lets.kkiri.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.lets.kkiri.common.util.RedisStoreUtil;
 import com.lets.kkiri.dto.chatting.MessageDto;
 import com.lets.kkiri.dto.chatting.MessageMetaData;
@@ -31,18 +33,8 @@ public class MessageRoomService {
     private final MessageRepositorySupport messageRepositorySupport;
 
     public void handleActions(WebSocketSession session, MoimSessionReq.MoimSessionType type, Object content, MessageService messageService) {
-        MessageDto msg = null;
-        switch (type) {
-            case MESSAGE:
-                msg = (MessageDto) content;
-                break;
-            case EMOJI:
-                msg = (MessageDto) content;
-                break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + type);
-        }
-
+        ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
+        MessageDto msg = mapper.convertValue(content, MessageDto.class);
         if(msg != null) {
             sendMessage(session, type, msg, messageService);
         }
@@ -52,34 +44,42 @@ public class MessageRoomService {
     }
 
     public <T> void sendMessage(WebSocketSession session, MoimSessionReq.MoimSessionType type, MessageDto msg, MessageService messageService) {
-        // Set<WebSocketSession> sessions = moimSessionListDto.getSessions();
         ArrayList<WebSocketSession> sessions = redisStoreUtil.getAllSessionsByMoimId(Long.parseLong(session.getAttributes().get("moimId").toString()), WebSocketSession.class);
         MessageSub sub = MessageSub.messageDtoToSub(msg);
         sub.setMessageType(type);
+        if(sub.getMessageType() == MoimSessionReq.MoimSessionType.URGENT) {
+            sub.setMoimId(Long.parseLong(session.getAttributes().get("moimId").toString()));
+        }
         sessions.parallelStream().forEach(s -> messageService.sendMessage(s, sub));
     }
 
     public MessageRes getFirstChat(Long moimId, Pageable pageable){
-        MessageRes res = new MessageRes();
         Page<Message> messages = messageRepositorySupport.findRecent(moimId, pageable);
-        MessageMetaData meta = MessageMetaData.builder()
-            .last(messages.isLast())
-            .lastMessageId(messages.getContent().get(messages.getContent().size()-1).getId())
-            .build();
-
-        List<MessageSub> msgList = new ArrayList<>();
-        for(Message msg : messages.getContent()){
-            MessageSub dto = MessageSub.messageToDto(msg);
-            msgList.add(dto);
-        }
-        res.setMeta(meta);
-        res.setChat(msgList);
+        MessageRes res = makeChatList(messages);
         return res;
     }
 
     public MessageRes getChat(Long moimId, String lastMessageId, Pageable pageable){
+        Page<Message> messages = messageRepositorySupport.findMessage(moimId, pageable, lastMessageId);
+        MessageRes res = makeChatList(messages);
+        return res;
+    }
 
-        return null;
+    private MessageRes makeChatList(Page<Message> msgList) {
+        MessageRes res = new MessageRes();
+        MessageMetaData meta = MessageMetaData.builder()
+            .last(msgList.isLast())
+            .lastMessageId(msgList.getContent().get(msgList.getContent().size()-1).getId())
+            .build();
+
+        List<MessageSub> msgsubList = new ArrayList<>();
+        for(Message msg : msgList.getContent()){
+            MessageSub dto = MessageSub.messageToDto(msg);
+            msgsubList.add(dto);
+        }
+        res.setMeta(meta);
+        res.setChat(msgsubList);
+        return res;
     }
 
 }
