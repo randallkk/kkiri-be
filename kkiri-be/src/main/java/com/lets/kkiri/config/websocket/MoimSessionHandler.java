ed65c4dafc592ff172675c1c895d57a450b29a4d
이지영 <jylisa0612@naver.com>
2023-05-12 17:35:10 +0900

    S08P31A606-278-be-web-socket-only-설정-확인 fix: MoimSessionHandler merge

--- kkiri-be/src/main/java/com/lets/kkiri/config/websocket/MoimSessionHandler.java
+++ kkiri-be/src/main/java/com/lets/kkiri/config/websocket/MoimSessionHandler.java
@@ -2,6 +2,7 @@ package com.lets.kkiri.config.websocket;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lets.kkiri.dto.moim.MoimSessionReq;
+import com.lets.kkiri.service.GpsService;
 import com.lets.kkiri.service.MessageRoomService;
 import com.lets.kkiri.service.MessageService;
 
@@ -9,6 +10,7 @@ import lombok.Builder;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -19,7 +21,6 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 import java.util.LinkedHashMap;
 
 @Slf4j
-@RequiredArgsConstructor
 @Component
 @Log4j2
 @RequiredArgsConstructor
@@ -29,28 +30,28 @@ public class MoimSessionHandler extends TextWebSocketHandler {
 	private final MessageService messageService;
 	private final MessageRoomService messageRoomService;
 	private WebSocketSession mySession;
-	// private final GpsService gpsService;
+	private final GpsService gpsService;
 
 	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
 		String payload = message.getPayload();
 		log.info("payload : {}", payload);
-
+		mySession = session;
 		MoimSessionReq msg = objectMapper.readValue(payload, MoimSessionReq.class);
 		Object content = msg.getContent();
 		log.debug("content : {}", content.toString());
 		log.debug("content type : {}", content.getClass());
 		switch (msg.getType()) {
 			case MESSAGE:
-
+				messageRoomService.handleActions(session, msg.getType(), content, messageService);
 				break;
 			case GPS:
 				gpsService.handleActions(session, content);
 				break;
 			case EMOJI:
 
+		}
 	}
-
 	public WebSocketSession getSession() {
 		return mySession;
 	}
