4ca2863e5a7951c26f7dd05767b06d9f419b329a
이지영 <jylisa0612@naver.com>
2023-05-12 09:59:12 +0900

    S08P31A606-278-be-web-socket-only-설정-확인 feat: send MessageType 수정

--- kkiri-be/src/main/java/com/lets/kkiri/service/MessageService.java
+++ kkiri-be/src/main/java/com/lets/kkiri/service/MessageService.java
@@ -1,6 +1,7 @@
 package com.lets.kkiri.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lets.kkiri.dto.chatting.MessageSub;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,10 +24,10 @@ public class MessageService {
     private final ObjectMapper objectMapper;
     private final MongoTemplate mongoTemplate;
 
-    public void sendMessage(WebSocketSession session, Object content)  {
+    public void sendMessage(WebSocketSession session, MessageSub msg)  {
         try{
-            session.sendMessage(new TextMessage(objectMapper.writeValueAsString(content)));
-            log.debug("sendMessage : {}", content);
+            session.sendMessage(new TextMessage(objectMapper.writeValueAsString(msg)));
+            log.debug("sendMessage : {}", msg);
             // mongoTemplate.save(content);
         }catch (Exception e){
             log.debug("sendMessage error : {}", e.getMessage());
