81f81df3a2b92a7d5a716c3d9ccd5de9679f2b4e
randallkk <randallkk.sw@gmail.com>
2023-05-26 00:15:15 +0900

    S08P31A606-308 fix: MemberGroupExpenseRepository 네이티브 쿼리로 수정

--- kkiri-be/src/main/java/com/lets/kkiri/repository/moim/MemberGroupExpenseRepository.java
+++ kkiri-be/src/main/java/com/lets/kkiri/repository/moim/MemberGroupExpenseRepository.java
@@ -2,6 +2,13 @@ package com.lets.kkiri.repository.moim;
 
 import com.lets.kkiri.entity.MemberGroupExpense;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface MemberGroupExpenseRepository extends JpaRepository<MemberGroupExpense, Long> , MemberGroupExpenseRepositorySupport {
+public interface MemberGroupExpenseRepository extends JpaRepository<MemberGroupExpense, Long> {
+    @Query(nativeQuery = true, value = "select sum(me.expense)" +
+            "from member_group_expense " +
+            "join moim_expense me on member_group_expense.moim_expense_id = me.id " +
+            "where me.moim_id = :moimId")
+    Integer findMoimExpenseByMoimId(@Param("moimId") Long moimId);
 }
