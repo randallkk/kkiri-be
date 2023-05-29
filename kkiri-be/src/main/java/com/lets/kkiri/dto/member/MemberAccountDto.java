4f42f60f498f6d1e7671473832a1ce92b0363b53
randallkk <randallkk.sw@gmail.com>
2023-05-29 11:47:57 +0900

    S08P31A606-308 feat: 계좌정보 조회, 추가 기능

---
+++ kkiri-be/src/main/java/com/lets/kkiri/dto/member/MemberAccountDto.java
@@ -0,0 +1,12 @@
+package com.lets.kkiri.dto.member;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MemberAccountDto {
+    private String accountUrl;
+}
