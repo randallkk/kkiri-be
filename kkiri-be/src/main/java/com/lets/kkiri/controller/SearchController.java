feeb9ca64de08dac2015bbb895090a764d5cc688
randallkk <randallkk.sw@gmail.com>
2024-07-08 05:41:16 +0900

    remove: kakao 연결 제거 #1

--- kkiri-be/src/main/java/com/lets/kkiri/controller/SearchController.java
+++ kkiri-be/src/main/java/com/lets/kkiri/controller/SearchController.java
@@ -1,29 +1,28 @@
-package com.lets.kkiri.controller;
-
-import com.lets.kkiri.dto.search.SearchPlaceRes;
-import com.lets.kkiri.service.KakaoSearchService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-@Slf4j
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/search")
-public class SearchController {
-    private final KakaoSearchService kakaoSearchService;
-
-    @GetMapping()
-    public SearchPlaceRes search(
-            @RequestParam String query,
-            @PageableDefault(size = 10, page = 1) Pageable pageable
-    ) {
-        return kakaoSearchService.searchPlace(query, pageable);
-    }
-}
+//package com.lets.kkiri.controller;
+//
+//import com.lets.kkiri.dto.search.SearchPlaceRes;
+//import com.lets.kkiri.service.KakaoSearchService;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.data.web.PageableDefault;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@Slf4j
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("/api/search")
+//public class SearchController {
+//    private final KakaoSearchService kakaoSearchService;
+//
+//    @GetMapping()
+//    public SearchPlaceRes search(
+//            @RequestParam String query,
+//            @PageableDefault(size = 10, page = 1) Pageable pageable
+//    ) {
+//        return kakaoSearchService.searchPlace(query, pageable);
+//    }
+//}
